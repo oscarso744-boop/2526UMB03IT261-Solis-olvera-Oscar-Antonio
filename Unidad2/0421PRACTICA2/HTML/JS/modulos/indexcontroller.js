0882426175bfd7cb75b77097898cae39a5ba8c68
oscarso744-boop <srojasmonroy334@gmail.com>
2026-04-27 15:18:11 -0600

    check

---
+++ Unidad2/0421PRACTICA2/HTML/JS/modulos/indexcontroller.js
@@ -0,0 +1,7 @@
+angular.module('app').controler('index', index);
+index.$inject = ['$scope', '$log'];
+function index($scope){
+    console.log('controlador')
+    $scope.mensaje1 = " hola soy una variable desde el controlador ";
+    
+}
